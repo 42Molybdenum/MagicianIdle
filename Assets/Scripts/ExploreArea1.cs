57dd9c293ca38fb1697dc03d7d7731664133b126
42Molybdenum <ro.richert@gmail.com>
2023-05-30 02:35:03 +0200

    Corrected last Addition

--- Assets/Scripts/ExploreArea1.cs
+++ Assets/Scripts/ExploreArea1.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ExploreArea1 : MonoBehaviour
+public class ExploreArea1 : Resource
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        resourceName = "ExploreArea1";
+        currentAmount = 0.0d;
+        storageAmount = 100.0d;
+        earningAmount = 0.1d;
+        consumptionAmount = 0.0d;
+        consumedResource = FindObjectOfType<FoodResource>();
     }
 
     // Update is called once per frame
